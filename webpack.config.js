e41a501e1d2d124fc600ade815a1b7f9f99989ed
Paulo Henrique <paulohenriquemr555@gmail.com>
2024-08-18 22:06:31 -0300

    Adding webpack, babel and react

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './assets/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
